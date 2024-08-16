b2f10972568ea05cea58ed29b4259c4cfb93bfa7
yenxill <dri1356@gmail.com>
2024-08-16 10:41:37 +0900

    < 2024. 08. 16 - session&cookie 수업 >

--- sample/src/main/java/egovframework/com/main/controller/MainController.java
+++ sample/src/main/java/egovframework/com/main/controller/MainController.java
@@ -1,9 +1,16 @@
 package egovframework.com.main.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,5 +40,50 @@ public class MainController {
 		mv.addObject("list", list); 
 		return mv;
 	}
-
+	
+	@RequestMapping("/sessionTest.do")
+	public ModelAndView sessionTest(HttpServletRequest request) {
+		ModelAndView mv = new ModelAndView("main/student");
+		
+		HttpSession session = request.getSession(false);
+		if(session == null) {
+			System.out.println("최초 방문입니다.");
+		} else {
+			String sessionId = session.getId();
+			System.out.println("세션 ID : " + sessionId);
+		}
+		
+		return mv;
+	}
+	
+	@RequestMapping("/sessionNumberTest.do")
+	public ModelAndView sessionNumberTest(HttpSession session) {
+		ModelAndView mv = new ModelAndView("main");
+		
+		Integer randomNumber = (Integer) session.getAttribute("random-number");
+		
+		if(randomNumber == null) {
+			Integer generated = new Random().nextInt();
+			System.out.println("generate random-number : " + generated.toString());
+			session.setAttribute("random-number", generated);
+			
+		} else {
+			System.out.println("my random-number : " + randomNumber.toString());
+		}
+		
+		return mv;
+	}
+	
+	@RequestMapping("/cookieTest.do")
+	public ModelAndView cookieTest(HttpServletResponse response) throws UnsupportedEncodingException {
+		ModelAndView mv = new ModelAndView("main");
+		
+		Cookie cookie = new Cookie("message", URLEncoder.encode("from main", "UTF-8"));
+		response.addCookie(cookie);
+		
+		return mv;
+	}
+	
+	
+	
 }
